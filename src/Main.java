8002b9c237f924dd88fc1be806a28e2b5cecb316
kozyar <kozyarik@gmail.com>
2023-06-08 20:34:57 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Напишите пример обработки нескольких исключений в одном блоке catch
+        Login login = new Login();
+        validate("login");
+    }
+
+    public static boolean validate(String login) {
+        try {
+            check(login);
+        } catch (NullPointerException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private static void check(String login) {
+        if (login == null) {
+            throw new NullPointerException("логин отсутствует!");
+        }
+        if (login.contains("login")) {
+            throw new IllegalArgumentException("логин не валидный!");
+        }
     }
 }
